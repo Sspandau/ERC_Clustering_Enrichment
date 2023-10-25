cecbe15385f7f593d40f95c7f9126e02e0fbd3af
Sspandau <114082773+Sspandau@users.noreply.github.com>
2023-10-24 18:00:41 -0700

    Update enrichment.py

--- enrichment.py
+++ enrichment.py
@@ -6,13 +6,13 @@ import numpy as np
 from functions_cluster import *
 
 @click.command()
-@click.option('--input', help="The path and name of the cluster csv from r scripts")
-@click.option('--output', help="The path and name of the output csv")
-@click.option('--clustering_type', default='cluster', help="Indicate whether enrichment is for clusters or subclusters")
+@click.option('--Input', help="The path and name of the cluster csv from r scripts")
+@click.option('--Output', help="The path and name of the output csv")
+@click.option('--clustering_level', default='cluster', help="Indicate whether enrichment is for clusters or subclusters")
 @click.option('--Filter', is_flag=True, help="Indicate whether to filter for top result in each cluster")
 
 
-df1 = pd.read_csv(cluster_csv)
+df1 = pd.read_csv(Input)
 
 df1 = df1.iloc[:, 1:]
 
@@ -24,14 +24,13 @@ go_cp = gp.get_library(name='GO_Cellular_Component_2021', organism='Human')
 kegg = gp.get_library(name='KEGG_2021_Human', organism='Human')
 wp = gp.get_library(name='WikiPathway_2021_Human', organism='Human')
 
-if clustering_type == "cluster":
+if clustering_level == "cluster":
     enr_bg = enrichment_clusters(df1, gene_list)
     if Filter:
         enr_bg = Filter(enr_bg, "cluster")
-elif clustering_type == "subcluster":
+elif clustering_level == "subcluster":
     enr_bg = enrichment_subclusters(df1, gene_list)
     if Filter:
         enr_bg = Filter(enr_bg, "subcluster")
         
-dfpct2.to_csv(output)
-    
+dfpct2.to_csv(Output)
